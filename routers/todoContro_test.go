a7cd53d5e1542954e27fdb1f24f7a2905db2f1a3
liuyong_awesome <a3831524@126.com>
2020-03-25 00:31:11 +0800

    调整样式

--- routers/todoContro_test.go
+++ routers/todoContro_test.go
@@ -100,7 +100,7 @@ func TestDeleteTodo(t *testing.T) {
 }
 
 func TestUpdateRule(t *testing.T) {
-	time.Sleep(3*time.Second)
+	time.Sleep(3 * time.Second)
 	//等待数据被添加完成
 	apiUrl := "/api/v1/todo/1?sort=1"
 	data := Get(apiUrl, router)
